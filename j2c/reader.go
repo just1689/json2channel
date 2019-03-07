35f0c5338e46a21793698448e344b4415318d76e
Justin Tamblyn <just1689@gmail.com>
2019-03-07 17:43:48 +0200

    Clean up

--- j2c/reader.go
+++ j2c/reader.go
@@ -1,26 +1,5 @@
 package j2c
 
-import (
-	"fmt"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
-)
-
-func ReadObjects(in Reader, element string) (out chan string) {
-	out = make(chan string)
-	go func() {
-		found := ReadUntilGivenElement(in, element)
-		if !found {
-			logrus.Error(errors.New(fmt.Sprint("Could not find", element, "in channel")))
-			close(out)
-			return
-		}
-		innerCh := ReadInnerArr(in)
-		ReadJsonObjects(innerCh, out)
-	}()
-	return out
-}
-
 func ReadUntilGivenElement(in Reader, element string) (found bool) {
 	found = false
 	var eof bool
@@ -63,68 +42,4 @@ func ReadUntilNextElement(in Reader) (found bool, word string, eof bool) {
 
 	}
 
-}
-
-func ReadInnerArr(in Reader) (out chan byte) {
-	out = make(chan byte)
-	go func() {
-		countLB := 0
-		countRB := 0
-		var c byte
-		var eof bool
-		for {
-			c, eof = in.Next()
-			if eof {
-				close(out)
-				return
-			}
-			if c == LB {
-				countLB++
-			} else if c == RB {
-				countRB++
-				if countLB == countRB {
-					close(out)
-					return
-				}
-			}
-			if countLB > 0 {
-				out <- c
-			}
-		}
-	}()
-	return out
-}
-
-func ReadJsonObjects(in chan byte, out chan string) {
-	go func() {
-		object := ""
-		countLS := 0
-		countRS := 0
-		reading := false
-		for c := range in {
-			if c == LS {
-				reading = true
-				countLS++
-			} else if c == RS {
-				countRS++
-				if countLS == countRS {
-					object += string(c)
-					out <- object
-					object = ""
-					reading = false
-					continue
-				}
-			} else if c == COMMA {
-				if countLS == countRS {
-					continue
-				}
-			}
-			if countLS > 0 && reading {
-				object += string(c)
-			}
-		}
-		close(out)
-		return
-
-	}()
 }
