06faee9bb3c71a362c19c1d3f73437260c090b21
Justin Tamblyn <just1689@gmail.com>
2019-03-07 17:26:08 +0200

    Clean up

--- j2c/reader-v2.go
+++ j2c/reader-v2.go
@@ -1,7 +1,5 @@
 package j2c
 
-import "fmt"
-
 func ReadObjectsV2(in Reader, element string) (out chan string) {
 	out = make(chan string)
 
@@ -27,7 +25,6 @@ func readArr(interpreter *Interpreter, out chan string) {
 	var b byte
 	var eof bool
 	word := ""
-	started := false
 	interpreter.ResetBrackets()
 	for {
 		b, eof = interpreter.Next()
@@ -47,30 +44,16 @@ func readArr(interpreter *Interpreter, out chan string) {
 			return
 		}
 
-		if b == LS && interpreter.ls == 1 {
-			//fmt.Println("started")
-			started = true
-		}
-
-		//fmt.Println(string(b), interpreter.ls, interpreter.rs)
-
 		if interpreter.ls == 0 {
 			continue
 		}
 
 		if interpreter.ls == interpreter.rs && b == RS {
 
-			if !started {
-				fmt.Println("Not going to send ", word)
-				continue
-			}
-
-			//fmt.Println("Writing to chan")
 			word += string(b)
 			out <- word
 			word = ""
 			interpreter.ResetSquiggly()
-			started = false
 
 			continue
 
